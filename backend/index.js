9b3a2dfcc47a7fceaa1ccb50f8a349ed8f9bba72
negi <negivishnu58@gmail.com>
2024-09-11 00:54:27 +0530

    changeurl

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `https://e-commerce-site-backend-ym9n.onrender.com/:${port}/images/${req.file.filename}`
+        image_url: `https://e-commerce-site-backend-ym9n.onrender.com:${port}/images/${req.file.filename}`
     })
 
 })
